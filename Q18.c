0912e1a59f4dac2d585f9214e1273b75ace36b17
khushanshu <khushanshuparihar@email.com>
2025-08-31 18:28:54 +0530

    hic

---
+++ Q18.c
@@ -0,0 +1,27 @@
+//Q18: Write a program to assign grades based on a percentage input.
+#include <stdio.h>
+int main() {
+float percentage;
+printf("Enter the percentage: ");
+scanf("%f", &percentage);
+if (percentage < 0 || percentage > 100) {
+printf("Invalid input. Percentage must be between 0 and 100.\n");
+} else {
+if (percentage >= 90) {
+printf("Grade: A+\n");
+} else if (percentage >= 80) {
+printf("Grade: A\n");
+} else if (percentage >= 70) {
+printf("Grade: B\n");
+} else if (percentage >= 60) {
+printf("Grade: C\n");
+} else if (percentage >= 50) {
+printf("Grade: D\n");
+} else if (percentage >= 40) {
+printf("Grade: E\n");
+} else {
+printf("Grade: F (Fail)\n");
+}
+}
+return 0;
+}
